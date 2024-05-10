c7dbdab077acbe6c28fadeede5ad562c2246e274
testxxxxxxxxx <pppppmarcin@gmail.com>
2024-05-10 22:36:37 +0200

    created data to benchmark

---
+++ benchmark/generate.py
@@ -0,0 +1,46 @@
+class File():
+
+    def __init__(self, path: str, name: str, mode: str) -> None:
+
+        self.__path: str = path
+        self.__name: str = name
+        self.__mode: str = mode
+
+        self.__dir: str = self.__path + self.__name
+
+    def save(self, value: int, fr: int, to: int) -> None:
+
+        with open(self.__dir, self.__mode) as f:
+
+            f.write(str(value) + "\n")
+            f.write(str(100) + "\n")
+            f.write("Find" + "\n")
+            f.write(str(fr) + "\n")
+            f.write(str(to) + "\n")
+            f.write("Khan" + "\n")
+            f.write("Tarjan" + "\n")
+            f.write("Exit" + "\n")
+
+class Generator():
+
+    def __init__(self, n: int) -> None:
+
+        self.__n: int = n
+
+    def generateToPath(self, path: str) -> str:
+
+        for i in range(1, self.__n + 1):
+
+            name: str = str(i)
+
+            file: File = File(path, name, 'w')
+            file.save(pow(2, i), pow(2, i) // 2, pow(2, i) - 1)
+
+def main() -> None:
+
+    generator: Generator = Generator(19)
+
+    generator.generateToPath('benchmark_results/')
+
+if __name__ == '__main__':
+    main()
--- include/functions.cpp
+++ include/functions.cpp
@@ -11,99 +11,14 @@ namespace Functions
 {
     int tableSize = 0;
 
-    /*int** generateGraph(int nodes, double saturation)
-    {
-        int **graph = new int*[nodes];
-        int numEdges = (nodes - 1) + (nodes * (nodes - 1) / 2) * saturation;
-        random_device rd;
-        mt19937 gen(rd());
-        uniform_int_distribution<> dist(0, nodes - 1);
-        int x = 0;
-        int y = 0;
-        int u = -1;
-        int w = -1;
-        int numEdgesE = 0;
-
-        for(int i = 0; i < nodes; i++)
-        {
-            graph[i] = new int[nodes];
-
-        }
-
-        for(int x = 0; x < nodes; x++)
-        {
-            for(int y = 0; y < nodes; y++)
-            {
-                graph[x][y] = 0;
-
-            }
-
-        }
-
-        std::vector<int> parent(nodes, -1);
-        std::vector<bool> inMST(nodes, false);
-        std::vector<double> weight(nodes, std::numeric_limits<double>::max());
-        weight[0] = 0;
-
-        for(int i = 0; i < nodes - 1; i++) 
-        {
-            u = -1;
-
-            for(int v = 0; v < nodes; v++) 
-            {
-                if(!inMST[v] && (u == -1 || weight[v] < weight[u])) 
-                    u = v;
-                
-            }
-            inMST[u] = true;
-            for(int v = 0; v < nodes; v++) 
-            {
-                if(!inMST[v] && dist(gen) < saturation)
-                {
-                    w = dist(gen) * 10;
-
-                    if(w < weight[v]) 
-                    {
-                        parent[v] = u;
-                        weight[v] = w;
-                    }
-                }
-            }
- 
-            for(int i = 1; i < nodes; ++i) 
-            {
-                graph[parent[i]][i] = 1;
-                graph[i][parent[i]] = 1;
-            }
-
-        }
-
-        numEdgesE = numEdges - (nodes  - 1);
-
-        for(int j = 0; j < numEdgesE; j++)
-        {
-            x = dist(gen);
-            y = dist(gen);
-
-            if(x != y && graph[x][y] != 0)
-            {
-                graph[x][y] = 1;
-
-            }
-
-        }
-
-        return graph;
-    }*/
     int** generateGraph(int n, double saturation) {
-        int** macierz = new int*[n]; // Tworzenie tablicy wskaźników na int
+        int** macierz = new int*[n];
         
         int edges = (n * (n - 1) / 2) * (saturation / 100);
         int howMany = 0;
         for (int i = 0; i < n; ++i) {
-            macierz[i] = new int[n]; // Tworzenie wierszy macierzy
-            for (int j = i + 1; j < n; ++j) { // Iteracja po górnej części macierzy
-                // Generowanie losowej wartości (0 lub 1) zgodnie z prawdopodobieństwem nasycenia
+            macierz[i] = new int[n]; 
+            for (int j = i + 1; j < n; ++j) {
                 if(howMany < edges)
                 {
                     macierz[i][j] = 1;
